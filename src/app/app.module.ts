70a25bc9d29e00208c05f2835686e27bc6f6e160
subaa1492 <60738126+subaa1492@users.noreply.github.com>
2022-03-22 19:16:28 +0530

    Diagram sample

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DiagramModule,
+    AppRoutingModule
+  ],
+  providers: [ ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
