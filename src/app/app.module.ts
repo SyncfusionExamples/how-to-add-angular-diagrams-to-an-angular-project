85a05bfa9b9c04a3d4427ce25542a4dac9ff8f04
mouliSF4071 <126136297+mouliSF4071@users.noreply.github.com>
2026-03-30 12:41:15 +0530

    1017348: dependabot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [    
   ],
   imports: [
     BrowserModule,
     DiagramModule,
-    AppRoutingModule
+    AppComponent
   ],
   providers: [ ],
   bootstrap: [AppComponent]
